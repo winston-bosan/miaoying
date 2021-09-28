74f7949855375694fe3487ffc3d0c8a3dbfecdb2
Winston Bosan <winston.bosan@gmail.com>
2021-09-27 23:09:02 -0400

    stagged!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./stronkcat.jpeg";
+import useSound from "use-sound";
+import "./App.css";
+import m from "./Miao.mp3";
+import M from "./Miaoo.mp3";
+import y from "./Ying.mp3";
+import Y from "./Yingg.mp3";
 
 function App() {
+
+  const BoopButton = () => {
+    const [play] = useSound(m);
+    return <button style={{ backgroundColor: "red" }} onClick={play}>Miao</button>;
+  };
+
+  const MButton = () => {
+    const [play1] = useSound(M);
+    return <button style={{ backgroundColor: "blue" }} onClick={play1}>MIiiiaaaooo!</button>;
+  };
+
+  const SyButton = () => {
+    const [play2] = useSound(y);
+    return <button  style={{ backgroundColor: "green" }} onClick={play2}>Ying</button>;
+  };
+
+  const YButton = () => {
+    const [play3] = useSound(Y);
+    return <button style={{ backgroundColor: "brown" }} onClick={play3}>YiiiiNNNGggggg!</button>;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="row">
+          <div className="column">{BoopButton()}</div>
+          <div className="column">
+           {MButton()}
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            {SyButton()}
+          </div>
+          <div className="column">
+            {YButton()}
+          </div>
+        </div>{" "}
+      </header>{" "}
     </div>
   );
 }
